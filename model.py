7f2d221349f04e75dd19c68e8855b9ef25d23204
qwtao <q13426591632@126.com>
2018-06-05 23:08:08 +0800

    add validation

--- model.py
+++ model.py
@@ -9,12 +9,13 @@ import json
 from torch.utils.data import DataLoader
 import numpy as np
 import time
+import rouge
 
 embed_path = "word2vec/embedding.npz"
 voca_path =  "word2vec/word2id.json"
 train_dir = "label_data/guardian_label/train/"
 val_dir = "label_data/guardian_label/valid/"
-batch_size = 32
+batch_size = 1
 learning_rate = 1e-3
 max_norm = 1.0
 sent_trunc = 20
@@ -147,29 +148,96 @@ train_iter = DataLoader(dataset = train_dataset,
                         collate_fn=my_collate)
 
 val_iter = DataLoader(dataset = val_dataset,
-                        batch_size = batch_size,
+                        batch_size = 1,
                         shuffle = False,
                       collate_fn=my_collate)
 
-critieion = nn.BCELoss()
+criterion = nn.BCELoss()
 optimizer = torch.optim.Adam(net.parameters(),lr=learning_rate)
 
-for epoch in range(0, 10):
-    for i, batch in enumerate(train_iter):
-        features, targets, _, doc_nums, doc_lens = vocab.make_features(batch, sent_trunc=sent_trunc, doc_trunc=doc_trunc)
+def validate(net, vocab, val_iter, criterion):
+    net.eval()
+    loss, p1, r1, f1 = 0, 0, 0, 0
+    batch_num = 0
+    r = rouge.Rouge()
+    r.metrics = ['rouge-1']
+
+    punc = [',','.','?','!',';',':']
+    def process_sent(s):
+        # 去掉句子s中的标点，以及将字母转为小写
+        for p in punc:
+            s = s.replace(p, '')
+        s =s.lower()
+        return s
+
+    for batch in val_iter:
+        features, targets, sents_content, gold_summary, doc_nums, doc_lens = vocab.make_features(batch, sent_trunc=sent_trunc,
+                                                                       doc_trunc=doc_trunc)
+        gold_summary = gold_summary[0] #返回的gold_summary是列表，而batch_size为1，故取第一个便是了
+
         features, targets = Variable(features), Variable(targets.float())
         if use_cuda:
             features = features.cuda()
             targets = targets.cuda()
         probs = net(features, doc_nums, doc_lens)
-        loss = critieion(probs, targets)
+        loss += criterion(probs, targets)
+
+        # 根据probs各分量的大小，选择得分最高的那部分的下标对应的句子作为摘要句
+        doc_nums = doc_nums[0]
+        probs = probs[doc_nums:] #去掉开头的文档标签预测
+        if use_cuda:
+            probs = probs.cpu()
+        probs = list(probs.detach().numpy())
+        sorted_index = list(np.argsort(probs)) #probs顺序排序后对应的下标
+        sorted_index.reverse()
+
+        gold_summary = process_sent(gold_summary)
+        ref_len =len(gold_summary.split()) # 参考摘要中的单词数
+        hyps_summary = ""
+        cur_len = 0
+        for index in sorted_index:
+            tmp = sents_content[index]
+            tmp = process_sent(tmp).split()
+            tmp_len = len(tmp)
+            if cur_len + tmp_len > ref_len:
+                hyps_summary += ' '.join(tmp[:ref_len-cur_len])
+                break
+            else:
+                hyps_summary += ' '.join(tmp)+' '
+                cur_len += tmp_len
+
+        rouge_1 = r.get_scores(hyps_summary, gold_summary)[0]['rouge-1']
+        p1 += rouge_1['p']
+        r1 += rouge_1['r']
+        f1 += rouge_1['f']
+
+        batch_num += 1
+
+    net.train()
+    return loss/batch_num, p1/batch_num, r1/batch_num, f1/batch_num
+
+
+for epoch in range(0, 10):
+    for i, batch in enumerate(train_iter):
+        features, targets, _1, _2, doc_nums, doc_lens = vocab.make_features(batch, sent_trunc=sent_trunc,
+                                                                       doc_trunc=doc_trunc)
+        features, targets = Variable(features), Variable(targets.float())
+        if use_cuda:
+            features = features.cuda()
+            targets = targets.cuda()
+        probs = net(features, doc_nums, doc_lens)
+        loss = criterion(probs, targets)
         optimizer.zero_grad()
         loss.backward()
         clip_grad_norm_(net.parameters(), max_norm)
         optimizer.step()
+        
+        # 在eval上计算loss，rouge-1的p、r、f值
 
         print("EPOCH %d: BATCH_ID=%d loss=%f time=%s"%(epoch, i, loss, time.ctime()))
-
+        if i%50 == 0:
+            eval_loss, p1, r1, f1 = validate(net, vocab, val_iter, criterion)
+            print("EVAL----EPOCH %d: BATCH_ID=%d loss=%f p1=%f r1=%f f1=%f time=%s" % (epoch, i, loss, p1, r1, f1, time.ctime()))
 
 
 
